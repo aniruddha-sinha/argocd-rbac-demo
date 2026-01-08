fca7ef5db5161502cc9b24e0404ca30caab33967
Aniruddha Sinha <asinha0493@gmail.com>
2026-01-08 15:49:24 +0530

    corrected the prep-script

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 build_yamls() {
     kustomize build ./argocd-installation > ./argocd-installation/argocd-install-final.yaml 
     kustomize build ./dev > ./dev/app-dev-final.yaml
-    kustomize build ./dev-platform > ./dev/app-dev-platform-final.yaml
+    kustomize build ./dev-platform > ./dev-platform/app-dev-platform-final.yaml
     kustomize build ./prod > ./prod/app-prod-final.yaml
     kustomize build ./argocd-users > ./argocd-users/argocd-cm-with-rbac-final.yaml
 }
